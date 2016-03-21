1a1b74ca662d5e0cfff54055f2922af001cc2638
Carlos Martínez Loyola <cmartinez@simplelabs.io>
2016-03-21 23:24:22 +1100

    Web base project

---
+++ WebApp/WebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApp.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/buildsoft").Include(
+                        "~/Scripts/angular.js",
+                        "~/Scripts/angular-animate.js"//,
+
+                        //"~/Scripts/domain/core/core.module.js",
+                        //"~/Scripts/domain/core/dataService.js",
+                        //"~/Scripts/domain/matcher/matcher.module.js",
+                        //"~/Scripts/domain/matcher/matcher.js"
+                        ));
+
+        }
+    }
+}
